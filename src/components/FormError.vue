ac30d411e81d2d8cd9f7b6ea6bf2dd720c20295e
Minh Hien <hien.nguyenhominh@gmail.com>
2026-07-20 04:51:07 +0700

    refactor: add FormError component

---
+++ src/components/FormError.vue
@@ -0,0 +1,36 @@
+<!-- src/components/common/FormError.vue -->
+<template>
+  <div :class="[
+    reserveSpace ? 'min-h-6' : '',
+    wrapperClass,
+  ]">
+    <p v-if="message" class="text-sm font-medium italic text-red-500" role="alert" aria-live="polite">
+      <span v-if="showAsterisk" aria-hidden="true">*</span>
+      {{ message }}
+    </p>
+  </div>
+</template>
+
+<script setup>
+defineProps({
+  message: {
+    type: String,
+    default: '',
+  },
+
+  reserveSpace: {
+    type: Boolean,
+    default: false,
+  },
+
+  showAsterisk: {
+    type: Boolean,
+    default: true,
+  },
+
+  wrapperClass: {
+    type: String,
+    default: 'mt-1',
+  },
+})
+</script>
